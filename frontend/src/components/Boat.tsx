9a7219057470c092ad2a9333f4a8bec3907b2dad
squach28 <squach3443@gmail.com>
2024-11-04 13:43:38 -0800

    allow paddlers on lineup page to be dragged to boat

--- frontend/src/components/Boat.tsx
+++ frontend/src/components/Boat.tsx
@@ -1,45 +1,43 @@
-import { useState } from "react";
+import React from "react";
+import { Paddler } from "../types/Paddler";
 
-const Boat = () => {
-  const [name, setName] = useState<string>("");
-  const [lefts, setLefts] = useState<Array<string>>([
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-  ]);
-  const [rights, setRights] = useState<Array<string>>([
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-    "Paddler",
-  ]);
+interface BoatProps {
+  name: string;
+  lefts: Array<Paddler | null>;
+  rights: Array<Paddler | null>;
+}
+
+const Boat = (boatProps: BoatProps) => {
+  const handleRowDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData("message");
+    e.currentTarget.textContent = data;
+  };
 
   return (
     <div className="bg-red-500 p-4 grid grid-cols-3 gap-2 grid-rows-12 justify-items-center justify-center min-w-36 min-h-96 rounded-2xl">
       <div className="row-start-1 col-start-1 col-span-3 text-center">
         Drummer
       </div>
-      {lefts.map((row, index) => {
-        return (
+      {boatProps.lefts.map((row, index) => {
+        return row ? (
           <div
             key={`${row} ${index}`}
             className={`col-start-1 row-start-${index + 2}`}
           >
-            <p className="row-start-1">{row}</p>
+            <p className="row-start-1">{row.fullName}</p>
           </div>
+        ) : (
+          <div
+            key={`${index}`}
+            className="min-w-20 border p-2"
+            onDragOver={handleRowDragOver}
+            onDrop={handleDrop}
+          ></div>
         );
       })}
       <div className="row-start-2 col-start-2">1</div>
@@ -52,14 +50,16 @@ const Boat = () => {
       <div className="row-start-9 col-start-2">8</div>
       <div className="row-start-10 col-start-2">9</div>
       <div className="row-start-11 col-start-2">10</div>
-      {rights.map((row, index) => {
-        return (
+      {boatProps.rights.map((row, index) => {
+        return row ? (
           <div
             key={`${row} ${index}`}
-            className={`col-start-3 row-start-${index + 2}`}
+            className={`col-start-1 row-start-${index + 2}`}
           >
-            <p className="row-start-1">{row}</p>
+            <p className="row-start-1">{row.fullName}</p>
           </div>
+        ) : (
+          <div key={`${index}`} className="min-w-20 border p-2"></div>
         );
       })}
       <div className="row-start-12 col-start-1 col-span-3 text-center">
--- frontend/src/types/Lineup.ts
+++ frontend/src/types/Lineup.ts
@@ -1,4 +1,8 @@
+import { Paddler } from "./Paddler";
+
 export type Lineup = {
   id: string;
   name: string;
+  lefts: Array<Paddler | null>;
+  rights: Array<Paddler | null>;
 };
